5ac45f498863a273d9c5a6353277c4ea2197a03d
Nerijus91 <108756394+Nerijus91@users.noreply.github.com>
2022-07-11 20:11:26 +0200

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name;
+Scanner sc = new Scanner(System.in);
+System.out.println("may I know your name");
+name= sc.nextLine();
+System.out.println("Welcome " + name);
+	}
+
+}
